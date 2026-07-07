bacbc30a40925368f15aed35740d9de0344bfb94
miks-qa <254993802+miks-qa@users.noreply.github.com>
2026-07-07 19:27:23 -0400

    Add project files.

---
+++ SeleniumCSharpDemo/PageObjects/loginPage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace SeleniumCSharpDemo.Pages
+{
+    public class loginPage
+    {
+        private readonly IWebDriver driver;
+
+        public loginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+
+        // Locators
+
+        private readonly By usernameField = By.Id("user-name");
+        private readonly By passwordField = By.Id("password");
+        private readonly By loginButton = By.Id("login-button");
+        private readonly By errorMessage = By.CssSelector("[data-test='error']");
+
+
+        // Actions
+
+        public void Open()
+        {
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+        }
+
+
+        public void EnterUsername(string username)
+        {
+            driver.FindElement(usernameField)
+                  .SendKeys(username);
+        }
+
+
+        public void EnterPassword(string password)
+        {
+            driver.FindElement(passwordField)
+                  .SendKeys(password);
+        }
+
+
+        public void ClickLogin()
+        {
+            driver.FindElement(loginButton)
+                  .Click();
+        }
+
+
+        public string GetErrorMessage()
+        {
+            return driver.FindElement(errorMessage).Text;
+        }
+
+
+        // Combined action
+        public void Login(string username, string password)
+        {
+            EnterUsername(username);
+            EnterPassword(password);
+            ClickLogin();
+        }
+    }
+}
---
+++ SeleniumCSharpDemo/Tests/loginTests.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumCSharpDemo.Pages;
+
+namespace SeleniumCSharpDemo
+{
+    [TestFixture]
+    public class loginTests
+    {
+        private IWebDriver driver;
+        private loginPage loginPageObject;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+
+            driver.Manage()
+                  .Window
+                  .Maximize();
+
+            loginPageObject = new loginPage(driver);
+        }
+
+
+        [Test]
+        public void Scenario1_ValidLogin()
+        {
+            // Scenario 1 - Login with valid credentials
+
+            loginPageObject.Open();
+            loginPageObject.Login("standard_user", "secret_sauce");
+
+            Assert.That(
+                driver.Url,
+                Does.Contain("inventory")
+            );
+        }
+
+
+        [Test]
+        public void Scenario2_InvalidLogin()
+        {
+            // Scenario 2 - Login with invalid credentials
+
+            loginPageObject.Open();
+            loginPageObject.Login("standard_user","wrong_password");
+
+            Assert.That(
+                loginPageObject.GetErrorMessage(),
+                Does.Contain("Username and password do not match")
+            );
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+                driver.Dispose();
+            }
+        }
+    }
+}
